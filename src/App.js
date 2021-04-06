aa70d25a703e27150bd9dcba247517737617e3d5
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2021-04-06 12:19:43 -0700

    Updated some files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 // This will act as the landing page for now, might change later
 
 // Necessary imports
-import logo from './logo.svg';
 import './App.css';
 
 // Rendering for the page
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Movie Finder </h1>
+      <p> Search bar and submit button will be added later </p>
     </div>
   );
 }
