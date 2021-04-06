d8af408ed1554fbf89a8943c478a3ff7f0fc6045
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2021-04-06 11:57:34 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+// This will act as the landing page for now, might change later
+
+// Necessary imports
 import logo from './logo.svg';
 import './App.css';
 
+// Rendering for the page
 function App() {
   return (
     <div className="App">
