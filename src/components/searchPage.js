d27d69649eadc3356721039fafcb06925f52c8bc
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2021-04-15 11:15:30 -0700

    Updated searchPage.js

--- src/components/searchPage.js
+++ src/components/searchPage.js
@@ -4,6 +4,55 @@
 import React, { Component } from 'react';
 
 export default class searchPage extends Component {
+
+    // Constructor to hold default states
+    constructor( props ) {
+        super( props );
+
+        // Binding functions
+
+        // Setting default state of values
+        this.state = {
+            title : ""
+        }
+    }
+
+    // Function to handle state of movie value -- constantly updating the value
+    onChangeMovieTitle( event ) {
+        this.setState( {
+            title: event.target.value
+        });
+    }
+
+    // Function to handle submission 
+    onHandleSubmit( event ) {
+        // Called to prevent page from reloading after user submits form
+        event.preventDefault();
+
+        // Assign user input to variable for movie title
+        var title = this.state.title;
+
+        // Call function to send request with given title to OMDB API
+        this.onComponentDidMount( title );
+
+        // Set default state of variable again
+        this.setState( {
+            title : ""
+        });
+    }
+
+    // Function to send request to OMDB API and display response to user
+    onComponentDidMount( title ) {
+        // Try and catch blocks -- send request to API via Axios, catch any errors that occur
+        try {
+
+        }
+        catch( error ) {
+            console.error( "An error has occurred.", error );
+        }
+    }
+
+    // Function to render form to user and response from API
     render() {
         return (
             <div className = "form">
