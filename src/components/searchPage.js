8bf1c9e3fb70e1024ff015336e26433600254989
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2021-04-20 12:22:10 -0700

    Added dynamically generated table functionality

--- src/components/searchPage.js
+++ src/components/searchPage.js
@@ -52,15 +52,25 @@ export default class searchPage extends Component {
             var key = process.env.REACT_APP_API_KEY;
             const result = [];
             axios.get( "http://www.omdbapi.com/?apikey=" + key + "&t=" + title )
-            // Take data from JSON response and insert into an array -- GET REST TOMORROW
+            // Take data from JSON response and insert into an array
             //.then( response => console.log( response ) )
             .then( response => {
                 const newItem = {
                     actors : response.data.Actors,
-                    awards : response.data.Awards
+                    awards : response.data.Awards,
+                    boxOffice : response.data.BoxOffice,
+                    director : response.data.Director,
+                    genre : response.data.Genre,
+                    metascore : response.data.Metascore,
+                    plot : response.data.Plot,
+                    rated : response.data.Rated,
+                    released : response.data.Released,
+                    runtime : response.data.Runtime,
+                    imdbRating : response.data.imdbRating
                 };
                 result.push( newItem );
                 console.log( result );
+                this.generateTable( result );
             })
             .catch( function( error ) {
                 console.log( error )
@@ -71,9 +81,41 @@ export default class searchPage extends Component {
         }
     }
 
-    // Function to dynamically generate table from given array of movie info
-    generateTable() {
+    // Function to dynamically generate table from given array of movie info -- DISPLAY MOVIE POSTER WHEN RETURNING TO PROJECT
+    generateTable( array ) {
+        // Start off by creating a table to later be appended
+        console.log( array );
+         var table = document.createElement( "table" );
 
+         // Array of headers to be displayed as table headers
+         var headers = [ "Actors", "Awards", "Box Office", "Director", "Genre", "ImdbRating",
+                         "Plot", "Rated", "Released", "Runtime", "Metascore" ];
+         var tr = table.insertRow( -1 );
+
+         // Add headers to table
+         for( var index = 0; index < headers.length; index++ ) {
+             var th = document.createElement( "th" );
+             th.innerHTML = headers[ index ];
+             tr.appendChild( th );
+         }
+
+         console.log( "Array length: " + array.length );
+         // Add data from JSON array to table
+         var newArray = array[0];
+         console.log( newArray );
+         let keys = Object.keys( newArray );
+         for( var index2 = 0; index2 < array.length; index2++ ) {
+             tr = table.insertRow( -1 );
+             for( var index3 = 0; index3 < headers.length; index3++ ) {
+                 var tableCell = tr.insertCell( -1 );
+                 tableCell.innerHTML = newArray[ keys[ index3 ] ];
+             }
+         }
+
+         // Add table to page
+         var divShowData = document.getElementsByClassName( "showData" );
+         divShowData.innerHTML = "";
+         divShowData[0].appendChild( table );
     }
 
     // Function to render form to user and response from API
@@ -94,6 +136,7 @@ export default class searchPage extends Component {
                         <input type = "submit" value = "Submit" className = "submitButton"/>
                     </div>
                 </form>
+                <p className = "showData"> </p>
             </div>
         )
     }
