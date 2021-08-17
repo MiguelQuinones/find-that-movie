16976821c9f5c076e4e8164555081552ac89aafd
MiguelQuinones <42755523+MiguelQuinones@users.noreply.github.com>
2021-08-17 13:38:06 -0700

    Finished layout for current movies list

--- src/components/moviesPlayingNow.js
+++ src/components/moviesPlayingNow.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-//import Carousel from "react-bootstrap/Carousel";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import axios from "axios";
 
 export default class PlayingNowSlides extends Component {
@@ -8,23 +10,7 @@ export default class PlayingNowSlides extends Component {
 
         // Other initializing stuff
         this.state = {
-            message : "",
-            firstMovieTitle : "",
-            firstMoviePoster : "",
-            secondMovieTitle : "",
-            secondMoviePoster : "",
-            thirdMovieTitle : "",
-            thirdMoviePoster : "",
-            fourthMovieTitle : "",
-            fourthMoviePoster : "",
-            fifthMovieTitle : "",
-            fifthMoviePoster : "",
-            sixthMovieTitle : "",
-            sixthMoviePoster : "",
-            seventhMovieTitle : "",
-            seventhMoviePoster : "",
-            eighthMovieTitle : "",
-            eigthMoviePoster : ""
+            moviesArray : []
         }
     }
 
@@ -35,28 +21,13 @@ export default class PlayingNowSlides extends Component {
 
             // Send request to API to get upcoming movies
             let response = await axios.get( "https://api.themoviedb.org/3/movie/now_playing?api_key=" + key + "&language=en-US&page=1" );
-            console.log( "Movies Now Playing: " );
-            console.log( response );
 
             // Store movie info for carousel slides
             this.setState( {
-                firstMovieTitle : response.data.results[ 0 ].title,
-                firstMoviePoster : response.data.results[ 0 ].backdrop_path,
-                secondMovieTitle : response.data.results[ 1 ].title,
-                secondMoviePoster : response.data.results[ 1 ].backdrop_path,
-                thirdMovieTitle : response.data.results[ 2 ].title,
-                thirdMoviePoster : response.data.results[ 2 ].backdrop_path,
-                fourthMovieTitle : response.data.results[ 3 ].title,
-                fourthMoviePoster : response.data.results[ 3 ].backdrop_path,
-                fifthMovieTitle : response.data.results[ 4 ].title,
-                fifthMoviePoster : response.data.results[ 4 ].backdrop_path,
-                sixthMovieTitle : response.data.results[ 5 ].title,
-                sixthMoviePoster : response.data.results[ 5 ].backdrop_path,
-                seventhMovieTitle : response.data.results[ 6 ].title,
-                seventhMoviePoster : response.data.results[ 6 ].backdrop_path,
-                eigthMovieTitle : response.data.results[ 7 ].title,
-                eigthMoviePoster : response.data.results[ 7 ].backdrop_path
+                moviesArray : response.data.results,
             } );
+            console.log( "Array info: " );
+            console.log( this.state.moviesArray );
 
         }
         catch( error ) {
@@ -66,103 +37,20 @@ export default class PlayingNowSlides extends Component {
 
     // Render 8 slides based on movies currently in theaters from API
     render() {
+        const movieData = this.state.moviesArray;
         return(
-            // <div className = "container" style = { { display : 'block', width : 800, padding : 30 } }>
-            //     <h1> Currently Playing <span className = "text-info"> Movies </span> </h1>
-            //     <br></br>
-            //     <Carousel nextLabel = "" prevLabel = "" fade = "true">
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.firstMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.firstMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.secondMoviePoster } alt="Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.secondMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.thirdMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.thirdMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.fourthMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.fourthMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.fifthMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.fifthMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.sixthMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.sixthMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.seventhMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.seventhMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-
-            //         <Carousel.Item>
-            //             <img className = "d-block w-100" src = { "https://image.tmdb.org/t/p/w780" + this.state.eigthMoviePoster } alt = "Slide"/>
-            //             <Carousel.Caption>
-            //                 <h3> { this.state.eigthMovieTitle } </h3>
-            //             </Carousel.Caption>
-            //         </Carousel.Item>
-            //     </Carousel>
-            // </div>
-            <div className = "container" style = { { textAlign : "center" } }>
-                <div className = "row">
-                    <div className = "col-lg-4">
-                        Why
-                    </div>
-                    <div className = "col-lg-4">
-                        Hello 
-                    </div>
-                    <div className = "col-lg-4">
-                        There
-                    </div>
-                </div>
-                <div className = "row">
-                    <div className = "col-lg-4">
-                        Why
-                    </div>
-                    <div className = "col-lg-4">
-                        Hello
-                    </div>
-                    <div className = "col-lg-4">
-                        There
-                    </div>
-                </div>
-                <div className = "row">
-                    <div className = "col-lg-4">
-                        Why
-                    </div>
-                    <div className = "col-lg-4">
-                        Hello
-                    </div>
-                    <div className = "col-lg-4">
-                        There
-                    </div>
-                </div>
-            </div>
+            <Row xs = { 1 } md = { 3 } className = "g-4">
+                { Array.from( movieData ).map( ( movie, idx ) => (
+                    <Col>
+                        <Card>
+                            <Card.Img variant = "bottom" src = { "https://image.tmdb.org/t/p/w500" + movie.poster_path } />
+                            <Card.Body>
+                                <Card.Title style = { { textAlign : "center" } }> { movie.title } </Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
         );
     }
 }
